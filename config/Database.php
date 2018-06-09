8be3ed5ba00790cf8e6252dc721c5b320cc9a3b1
IndranilRay <36516665+IndranilRay@users.noreply.github.com>
2018-06-10 00:05:18 +0530

    remove debug statement

--- config/Database.php
+++ config/Database.php
@@ -26,13 +26,11 @@ class Database
     }
 
     public static function execute($stmt){
-
-
+        //$stmt->execute();
+        //echo '<pre>';
+        //print_r( $stmt );
+        //exit;
         if (!$stmt->execute()) {
-            echo '<pre>';
-            print_r($stmt);
-            exit;
-
             echo "\nPDO::errorInfo():\n";
             print_r($stmt->errorInfo());
             die(1);
